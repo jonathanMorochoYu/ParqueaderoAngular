9e2385c4066c482963acf0baf07edf99a2e26829
jonathanMorocho2 <73446057+jonathanMorocho2@users.noreply.github.com>
2023-08-02 17:59:22 -0500

    arreglado  persona y vehiculo

--- ParqueaderoAngular/src/app/page/persona/persona.component.ts
+++ ParqueaderoAngular/src/app/page/persona/persona.component.ts
@@ -10,47 +10,71 @@ import { PersonaService } from 'src/app/services/persona.service';
 })
 export class PersonaComponent {
 
+
   persona: Persona = new Persona();
 
-  constructor(private personaService: PersonaService,
-    private router: Router) {
-
-      let params = this.router.getCurrentNavigation()?.extras.queryParams;
-      if(params){
-        console.log(params)
-        this.persona = new Persona()
-        this.persona = params['persona']
-      }
+  constructor(
+    private personasService: PersonaService,
+    private router: Router
+  ) {
+    let params = this.router.getCurrentNavigation()?.extras.queryParams;
+    if (params) {
+      console.log(params);
+      this.persona = new Persona();
+      this.persona = params['persona'];
     }
+  }
 
+  enviar() {
+    console.log(this.persona);
+    if (!isNaN(Number(this.persona.cedulaPer)) && this.persona.cedulaPer.length === 10) {
 
+    this.personasService.save(this.persona).subscribe(data => {
+      console.log("Resultado WS save", data);
+      this.reloadPage();
+    });
+  }else{
+    alert("Nro. Cedula Incorrecta")
+  }
+  }
 
+  actualizar() {
+    console.log(this.persona);
+    if (!isNaN(Number(this.persona.cedulaPer)) && this.persona.cedulaPer.length === 10) {
+    this.personasService.save(this.persona).subscribe(data => {
+      console.log("Resultado WS update", data);
+      this.router.navigate(["listar"])
+      //this.reloadPage();
+    });
+  }else{
+    alert("Nro. Cedula Incorrecta")
+  }
+  }
 
-
-    guardar() {
-      console.log(this.persona)
-      //this.contactoService.save(this.contacto)
-      //this.contacto = new Contacto()
-      if (!isNaN(Number(this.persona.cedulaPer)) && this.persona.cedulaPer.length === 10) {
-
-      this.personaService.save(this.persona).subscribe(data =>{
-        console.log("resultado WS save", data)
-      });
-
-      this.persona = new Persona()
-      }else {
-        alert("Nro. Cedula Incorrecta")
-      }
-    }
-    Edita2() {
-      console.log(this.persona)
-      this.personaService.save(this.persona)
-      this.persona = new Persona()
-      this.personaService.save(this.persona).subscribe(data =>{
-        console .log("resultado WS save", data)
-      });
-      this.persona = new Persona()
-    } 
-  
-    
+  reloadPage() {
+    window.location.reload();
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ParqueaderoAngular/src/app/page/vehiculo/vehiculo.component.ts
+++ ParqueaderoAngular/src/app/page/vehiculo/vehiculo.component.ts
@@ -9,47 +9,62 @@ import { VehiculoService } from 'src/app/services/vehiculo.service';
   styleUrls: ['./vehiculo.component.scss']
 })
 export class VehiculoComponent {
+
+
   vehiculo: Vehiculo = new Vehiculo();
 
-  constructor(private vehiculoService: VehiculoService,
-    private router: Router) {
-
-      let params = this.router.getCurrentNavigation()?.extras.queryParams;
-      if(params){
-        console.log(params)
-        this.vehiculo = new Vehiculo()
-        this.vehiculo = params['vehiculo']
-      }
+  constructor(
+    private vehiculoService: VehiculoService,
+    private router: Router
+  ) {
+    let params = this.router.getCurrentNavigation()?.extras.queryParams;
+    if (params) {
+      console.log(params);
+      this.vehiculo = new Vehiculo();
+      this.vehiculo = params['vehiculo'];
     }
+  }
 
+  enviar() {
+    console.log(this.vehiculo);
+    if (!isNaN(Number(this.vehiculo.placaVeh)) && this.vehiculo.placaVeh.length === 10) {
 
+    this.vehiculoService.save(this.vehiculo).subscribe(data => {
+      console.log("Resultado WS save", data);
+      this.reloadPage();
+    });
+  }else{
+    alert("Nro. Cedula Incorrecta")
+  }
+  }
 
+  actualizar() {
+    console.log(this.vehiculo);
+    if (!isNaN(Number(this.vehiculo.placaVeh)) && this.vehiculo.placaVeh.length === 5) {
+    this.vehiculoService.save(this.vehiculo).subscribe(data => {
+      console.log("Resultado WS update", data);
+      this.router.navigate(["listarveh"])
+      //this.reloadPage();
+    });
+  }else{
+    alert("Placa Incorrecta")
+  }
+  }
 
-
-    guardar() {
-      console.log(this.vehiculo)
-      //this.contactoService.save(this.contacto)
-      //this.contacto = new Contacto()
-      if (!isNaN(Number(this.vehiculo.codigoVeh)) && this.vehiculo.codigoVeh.length === 10) {
-
-      this.vehiculoService.save(this.vehiculo).subscribe(data =>{
-        console.log("resultado WS save", data)
-      });
-
-      this.vehiculo = new Vehiculo()
-      }else {
-        alert("Nro. Cedula Incorrecta")
-      }
-    }
-    Edita2() {
-      console.log(this.vehiculo)
-      this.vehiculoService.save(this.vehiculo)
-      this.vehiculo = new Vehiculo()
-      this.vehiculoService.save(this.vehiculo).subscribe(data =>{
-        console .log("resultado WS save", data)
-      });
-      this.vehiculo = new Vehiculo()
-    } 
-  
-    
+  reloadPage() {
+    window.location.reload();
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
